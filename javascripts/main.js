d7822518b990303b63624dda274d678ef08d22f2
三橋啓多 <keita.mitsuhashi.83@gmail.com>
2016-08-21 14:47:58 +0900

    update contents

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,14 @@
 $(function() {
+  var hash = location.hash.substr(1);
+  var content_id = '#content_' + hash;
+  var selectedClassStr = 'selected';
+  $(content_id).addClass(selectedClassStr);
+  setTimeout(function(){
+    $(content_id).fadeIn(1000);
+  },4000);
+
   $('#tmenu td').click(function(){
-    var selectedClassStr = 'selected';
-    var content_id = '#content_' + this.id;
+    content_id = '#content_' + this.id;
     $('#tmenu').find('a').removeClass(selectedClassStr);
     $('#content_wrap').find('.content').removeClass(selectedClassStr);
     $(this).find('a').addClass(selectedClassStr);
