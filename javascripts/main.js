ed706f0bdb6093e54051370875de7034f96e065e
三橋啓多 <keita.mitsuhashi.83@gmail.com>
2016-08-21 18:15:46 +0900

    update some

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,5 +1,8 @@
 $(function() {
   var hash = location.hash.substr(1);
+  if(hash){
+    $('#greeting').hide();
+  }
   var content_id = '#content_' + hash;
   var selectedClassStr = 'selected';
   $(content_id).addClass(selectedClassStr);
@@ -8,6 +11,7 @@ $(function() {
   },4000);
 
   $('#tmenu td').click(function(){
+    $('#greeting').fadeOut(1000);
     content_id = '#content_' + this.id;
     $('#tmenu').find('a').removeClass(selectedClassStr);
     $('#content_wrap').find('.content').removeClass(selectedClassStr);
