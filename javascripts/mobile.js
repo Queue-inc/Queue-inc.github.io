b8ec569470b72d0b5e4a80063aa7cfd1d6cbd97a
三橋啓多 <keita.mitsuhashi.83@gmail.com>
2016-08-22 17:48:52 +0900

    add mobile styles

--- javascripts/mobile.js
+++ javascripts/mobile.js
@@ -56,13 +56,13 @@ $(function() {
     }
 
     content_id = '#content_' + this.id;
+    $('#tmenu').find('a').removeClass(selectedClassStr);
     $('#content_wrap').find('.content').removeClass(selectedClassStr);
     $(this).find('a').addClass(selectedClassStr);
     $(content_id).addClass(selectedClassStr);
     $('#content_wrap').find('.content').fadeOut(1000);
     $(content_id).fadeIn(1000);
 
-    $('#sp_menu').toggleClass('openNav');
     $('#sp_menu').find('nav').toggleClass('selected');
   });
 
